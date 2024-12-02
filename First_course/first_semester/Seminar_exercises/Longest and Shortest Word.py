46846ed9f2f81d47bdb18d967b32664d3d1f7b31
VladimirTsanov <vtsanov2005@gmail.com>
2024-12-02 20:56:05 +0200

    Create Longest and Shortest Word.py

---
+++ First_course/first_semester/Seminar_exercises/Longest and Shortest Word.py
@@ -0,0 +1,7 @@
+text = input("Въведи изречение:")
+splitted_sentence = text.split()
+res1 = max(splitted_sentence, key=len)
+res2 = min(splitted_sentence, key=len)
+
+print("Max word: " + res1)
+print("Min word: " + res2)
