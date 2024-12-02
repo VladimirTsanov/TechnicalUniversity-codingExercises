9d8ea2a0fd18c4cfae93035945107c90b6537d83
VladimirTsanov <vtsanov2005@gmail.com>
2024-12-02 20:45:06 +0200

    Delete seminar_exercises directory

--- seminar_exercises/Longest and Shortest Word.py
+++
@@ -1,7 +0,0 @@
-text = input("Type a sentence: ")
-splitted_sentence = text.split()
-res1 = max(splitted_sentence, key=len)
-res2 = min(splitted_sentence, key=len)
-
-print("Max word: " + res1)
-print("Min word: " + res2)
